948027dd2324f2d1ef2a6e973230ac6e0efd891e
Antares <antarestauri@gmail.com>
2023-03-05 15:23:40 +0300

    Completed Navigation Drawer

--- app/src/main/java/com/antares/rline/presentation/MainActivity.kt
+++ app/src/main/java/com/antares/rline/presentation/MainActivity.kt
@@ -2,11 +2,44 @@ package com.antares.rline.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.antares.rline.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_main, R.id.nav_promotions,
+                R.id.nav_telephony, R.id.nav_contacts,
+                R.id.nav_support, R.id.nav_share, R.id.nav_exit
+            ), findViewById(R.id.drawer_layout)
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.nav_view)?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
